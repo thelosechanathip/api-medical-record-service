e93daa79b7fbc62056cfd5a4d6ca47709817ee53
thelosechanathip <thelosechanathip1996@gmail.com>
2025-09-30 13:24:03 +0700

    edit

--- src/modules/setting/hcode/hcode.controller.js
+++ src/modules/setting/hcode/hcode.controller.js
@@ -32,13 +32,7 @@ exports.UpsertHcode = async (req, res) => {
             hcodeData.updated_by = req.fullname
         }
 
-        const startTime = Date.now()
         const UHc = await chm.UpsertHcode(hcodeId[0], hcodeData)
-        const endTime = Date.now() - startTime
-
-        // Set and Insert Log
-        const sl = setLog(req, req.fullname, endTime, UHc)
-        await chm.InsertLog(sl)
 
         return msg(res, 200, { message: "Upsert data successfully!" })
     } catch (err) {
--- src/modules/setting/hcode/hcode.route.js
+++ src/modules/setting/hcode/hcode.route.js
@@ -4,7 +4,7 @@ const m = require('../../../middleware/auth/auth.controller') // m = middleware
 const hcc = require("./hcode.controller")
 
 router.get('/hcode', hcc.FetchAllHcodes)
-router.post('/hcode', m.CheckTokenUser, hcc.UpsertHcode)
+router.post('/hcode', hcc.UpsertHcode)
 router.get('/hcode/:hcodeId', m.CheckTokenUser, hcc.FetchOneHcodeById)
 router.delete('/hcode/:hcodeId', m.CheckTokenUser, hcc.RemoveHcode)
 
