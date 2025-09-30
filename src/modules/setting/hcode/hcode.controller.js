45cf33bcc36b4eaa8cab9d16a73e49424c64bf63
thelosechanathip <thelosechanathip1996@gmail.com>
2025-09-30 13:14:44 +0700

    edit

--- src/modules/setting/hcode/hcode.controller.js
+++ src/modules/setting/hcode/hcode.controller.js
@@ -5,15 +5,10 @@ const chm = require("./hcode.model")
 // Function FetchAll
 exports.FetchAllHcodes = async (req, res) => {
     try {
-        const startTime = Date.now()
         const FaHc = await chm.FetchAllHcodes()
-        const endTime = Date.now() - startTime
-
-        // Set and Insert Log
-        const sl = setLog(req, req.fullname, endTime, FaHc)
-        await chm.InsertLog(sl)
 
         if (FaHc.length === 0) return msg(res, 404, { message: 'Data not found!' })
+            
         return msg(res, 200, { message: "Fetch all data successfully!", data: FaHc })
     } catch (err) {
         console.log('FetchAllHcodes : ', err)
